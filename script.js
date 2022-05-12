7ecef90e259bc87a781ae60b72ff424f115bce9a
PeterBookmyer <101828030+PeterBookmyer@users.noreply.github.com>
2022-05-12 17:54:04 -0600

    More Basic Structure Added, Linked JS, Jquery, Bootstrap, added true and false button and prompt to start game....JS coming along

---
+++ script.js
@@ -0,0 +1,28 @@
+
+var userSelection = $("#userSelection");
+var userScore = $("#userScore");
+var startBtn = confirm("Would you like to take the quiz?");
+// var questionMain =
+// var gameTimer = ""
+var btnTrue = $("#btnTrue")
+var btnFalse = $("#btnFalse")
+
+if (startBtn){
+    
+}
+
+
+var quizQuestions = [
+{
+question: "Is the Sky Blue?",
+answer: true
+},
+{
+question: "Is Snow hot?",
+answer: false
+},
+{
+question: "Is the Earth Round?",
+answer: true
+}];
+
