61d19ac63a9744a1e933229f6cd799b69378b57b
PeterBookmyer <101828030+PeterBookmyer@users.noreply.github.com>
2022-05-12 22:28:14 -0600

    JS coming together...added countdown clock starting at 30 ending at 0, questions showing up in body el, true false buttons with functionality

--- script.js
+++ script.js
@@ -1,28 +1,53 @@
 
-var userSelection = $("#userSelection");
+var userTrue = $("#btnTrue");
+var userFalse= $("#btnFalse")
 var userScore = $("#userScore");
 var startBtn = confirm("Would you like to take the quiz?");
 // var questionMain =
-// var gameTimer = ""
+var gameTimer = $("#gameTimer");
+var secondsLeft = 30;
 var btnTrue = $("#btnTrue")
 var btnFalse = $("#btnFalse")
+var quizQuestionIndex = [0]
+var questionsMain = $("#questionsMain");
 
-if (startBtn){
-    
-}
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timerEl.textContent = secondsLeft
+if(secondsLeft === -1) {
+      clearInterval(timerInterval);
+      alert("You are Out of Time!");
+    }
 
+},1000);};
+
+setTime()
 
 var quizQuestions = [
-{
-question: "Is the Sky Blue?",
-answer: true
-},
-{
-question: "Is Snow hot?",
-answer: false
-},
-{
-question: "Is the Earth Round?",
-answer: true
-}];
+"Is Mt Elbert is the Highest Peak in Colorado?",
+"Is the Lark Bunting the State Bird of Colorado?",
+ "Does Colorado have Grizzly Bears?",
+ "Is the State Flower of Colorado the Columbine?",
+ "Is the Mascot of the University of Colorado the Rams?",
+];
+
+function gameFlow() {
+
+for (var i = 0; i < quizQuestions.length; i++) { 
+    questionsMain.text(quizQuestions[0]);
+    ;
+};
+btnTrue.on('click', function () {
+  questionsMain.text(quizQuestions[1]);
+});
+btnFalse.on('click', function () {
+  questionsMain.text(quizQuestions[2]);
+});
+}
+   
+gameFlow();
+
+
+
 
